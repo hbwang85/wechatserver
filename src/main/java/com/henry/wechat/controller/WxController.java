ac0b61ce30b064a6618804104b953a8d36c26a92
Haibin Wang <hbwang85@hotmail.com>
2016-11-27 20:40:17 +0800

    add project code

---
+++ src/main/java/com/henry/wechat/controller/WxController.java
@@ -0,0 +1,18 @@
+package com.henry.wechat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by w-teng on 2016/11/27.
+ */
+@Controller
+@RequestMapping("/")
+public class WxController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String home() {
+        return "index";
+    }
+}
